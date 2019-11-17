86a867aeec08c9418f378083e60374681c675ebf
MozyOk <mozy.okubo@gmail.com>
2019-11-17 09:00:44 +0200

    fix for heroku

--- main.py
+++ main.py
@@ -82,4 +82,7 @@ def get_by_categories():
   json_data = json.dumps(items, ensure_ascii=False, indent=2)
   return json_data  
 
-run(host='localhost', port=8080, debug=True)
+if os.environ.get('APP_LOCATION') == 'heroku':
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+    run(host='localhost', port=8080, debug=True)
